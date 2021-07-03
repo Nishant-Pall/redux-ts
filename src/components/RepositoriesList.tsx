2eeb9d09878bd399bb3d31bc82f1b59693560713
Nishant-Pall <palln6935@gmail.com>
2021-07-03 18:17:23 +0530

    Destructure and call the action

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { useActions } from "../hooks/useActions";
-import { searchRepositories } from "../state/action-creators";
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState("");
-    // const { searchRepositories } = useActions();
+    const { searchRepositories } = useActions();
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         searchRepositories(term);
