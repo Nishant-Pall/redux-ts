b2d6db6fa781a2669648da1ed9f8adf807c9f1ca
Nishant-Pall <palln6935@gmail.com>
2021-07-06 08:21:44 +0530

    Add documentation

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,8 +1,14 @@
+// ACTION CREATORS MAKE REQUEST LIKE THE GET
+// REQUEST IN THIS CASE
+
 import axios from "axios";
 import { Dispatch } from "redux";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
+// dispatch is used to manually dispatch
+// actions directly into the redux store
+// and get processed by the reducers
 export const searchRepositories = (term: string) => {
     return async (dispatch: Dispatch<Action>) => {
         dispatch({
