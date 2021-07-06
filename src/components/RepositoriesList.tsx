365b776860f16a852025e0520306efb11f6d4302
Nishant-Pall <palln6935@gmail.com>
2021-07-07 01:56:41 +0530

    Bridge components to src/index.tsx

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RepositoriesList = () => {
+    return <div></div>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(
-    <React.StrictMode></React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
     document.getElementById("root")
 );
 
