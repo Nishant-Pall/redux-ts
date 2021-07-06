e2605af00d6b948a6c5e22b74bbbc38b23d3dfdc
Nishant-Pall <palln6935@gmail.com>
2021-07-07 00:41:36 +0530

    Add documentation

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,8 +1,12 @@
+// Use this hook to bind the action creators to the dispatch function
+// and export it to the component for optimizing the code
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state";
 
 export const useActions = () => {
+    // Get access to dispatch function
+    // Used to dispatch an action to all the reducers
     const dispatch = useDispatch();
 
     // returns action creators and bind them to dispatch
