c4e47b531e50294dac859ff3593eec651aa547c0
Nishant-Pall <palln6935@gmail.com>
2021-07-06 08:53:09 +0530

    Add documentation

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,4 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 
+// reducers, initial state argumeny of an empty object,
+// apply middleware
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
